cd6441b853cf5f39ce45145b78d529097792c26f
EC2 Default User <ec2-user@ip-172-31-80-152.ec2.internal>
2021-01-31 01:27:33 +0000

    コントローラー作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  #strong parameter
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  
+  devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #homes routes
+  get '/'=>'public/homes#top'
+  get 'about'=>'public/homes#about'
+  #items routes
+  get 'items' => 'public/items#index'
+  get 'items/:id' =>'public/items#show'
+    
 end
